94616b360b7c0b330cb815cc9a5f5127f99bad66
Tamás Tóth <tamas516@gmail.com>
2022-05-22 18:48:24 +0200

    Relative Project 0.2

--- src/main/java/hu/nye/algrelatives/Main.java
+++ src/main/java/hu/nye/algrelatives/Main.java
@@ -3,6 +3,9 @@ package hu.nye.algrelatives;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Main class.
+ */
 @SpringBootApplication
 public class Main {
 
